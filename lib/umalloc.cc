464f72ecb2bad57cc6f13e1326902629a541733f
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-11-18 10:57:37 -0500

    make gcc 4.9 happy
    otherwise it complains that thread-local variables need dynamic
    initialization:
    
    lib/umalloc.cc:366:48: error: non-local variable ‘{anonymous}::free_runs’ declared ‘__thread’ needs dynamic initialization
       __thread block_list free_runs[MAX_LARGE_CLASS];
                                                    ^
    lib/umalloc.cc:366:23: note: C++11 ‘thread_local’ allows dynamic initialization and destruction
       __thread block_list free_runs[MAX_LARGE_CLASS];
                           ^
    lib/umalloc.cc:550:40: error: non-local variable ‘{anonymous}::free_fragments’ declared ‘__thread’ needs dynamic initialization
       __thread block_list free_fragments[13];
                                            ^

--- lib/umalloc.cc
+++ lib/umalloc.cc
@@ -363,7 +363,7 @@ namespace {
 
   // Page free lists indexed by size class.  The smaller size classes
   // are unused.
-  __thread block_list free_runs[MAX_LARGE_CLASS];
+  __thread block_list free_runs[MAX_LARGE_CLASS] = {};
 
   // Page info radix array.  The +1 on the size is a lame way to avoid
   // having to constantly check our iterators against pages.end().
@@ -547,7 +547,7 @@ namespace {
   // Fragment free lists by size class (ceil(log2(bytes))).  Fragments
   // must be at least sizeof(block_list::block), so the smaller
   // classes are unused.
-  __thread block_list free_fragments[13];
+  __thread block_list free_fragments[13] = {};
 
   // Header for pages owned by the small allocator.  Following this
   // header, a page is divided into equal-size fragments.
