d8138a47032a7eb4ad6c5ad54e21add213fb0990
Srivatsa S. Bhat <srivatsa@mit.edu>
2015-08-19 15:47:16 -0400

    bin/renamefsync: Fsync the parent directory to flush all the paths
    Under the new semantics for fsync, if the parent directory is not fsync()ed,
    the paths to the newly created files or directories underneath it will not
    be made persistent on the disk (and hence they won't be reachable by path
    names on the disk after a reboot).
    
    So fsync the parent directory in the test. [ We had skipped it because
    of a bug that used to crash the kernel earlier; but the previous commit
    fixed it. ]

--- bin/renamefsync.cc
+++ bin/renamefsync.cc
@@ -6,8 +6,11 @@
 #include <stdio.h>
 
 void fsync_run() {
-  int fd_dir1, fd_file1, fd_dir2;
+  int fd_cwd, fd_dir1, fd_file1, fd_dir2;
   char buf[10];
+
+  if ((fd_cwd = open(".", 0)) < 0)
+    die("error: could not open .");
 
   if (mkdir("testdir1", 0777) < 0)
     die("error: mkdir testdir1 failed");
@@ -15,6 +18,7 @@ void fsync_run() {
     die("error: could not open testdir1");
 
   fsync(fd_dir1);
+  fsync(fd_cwd);
 
   if (mkdir("testdir2", 0777) < 0)
     die("error: mkdir testdir2 failed");
@@ -22,6 +26,7 @@ void fsync_run() {
     die("error: could not open testdir2");
 
   fsync(fd_dir2);
+  fsync(fd_cwd);
 
   if ((fd_file1 = open("testdir1/testfile1", O_RDWR | O_CREAT)) < 0)
     die("error: could not open testfile1");
@@ -40,6 +45,7 @@ void fsync_run() {
   close(fd_file1);
   close(fd_dir1);
   close(fd_dir2);
+  close(fd_cwd);
 
   printf("renamefsync: success\n");
 }
