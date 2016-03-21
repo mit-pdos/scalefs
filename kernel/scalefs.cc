d3f234ac1d64593324e39edc6ae672bb14cbedab
Srivatsa S. Bhat <srivatsa@csail.mit.edu>
2016-03-21 17:03:49 -0400

    scalefs: Remove additional checks for inode's existence in create_file_dir_if_new()
    We don't invoke create_file_dir_if_new() unless we really want to allocate a new
    inode for the file/directory being created. So no need to check if they already
    have inodes, because they won't. These checks are remnants of an earlier design.

--- kernel/scalefs.cc
+++ kernel/scalefs.cc
@@ -177,21 +177,7 @@ u64
 mfs_interface::create_file_dir_if_new(u64 mnum, u64 parent_mnum, u8 type,
                                       transaction *tr)
 {
-  u64 inum = 0, parent_inum = 0;
-  if (inum_lookup(mnum, &inum)) {
-    // A file just needs its inode to be allocated, so it is safe to return
-    // here.
-    if (type == mnode::types::file)
-      return inum;
-
-    // A directory, on the other hand, should additionally have its ".." link
-    // initialized. Verify that it has been initialized, and if its not, then
-    // continue where we had left off.
-    char name[DIRSIZ];
-    strcpy(name, "..");
-    if (type == mnode::types::dir && dirlookup(iget(1, inum), name))
-        return inum;
-  }
+  u64 parent_inum = 0;
 
   // To create a new directory, we need to allocate a new inode as well as
   // initialize it with the ".." link, for which we need to know its parent's
