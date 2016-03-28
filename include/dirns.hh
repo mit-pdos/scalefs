461615b7460f9fde22468af469409dd9b9e1d921
Srivatsa S. Bhat <srivatsa@csail.mit.edu>
2016-03-27 20:50:02 -0400

    fs: Prepare to replace the dirns class
    Implement a new class to manage the on-disk directory entries using a chainhash,
    in preparation to replace dirns.

--- include/dirns.hh
+++ include/dirns.hh
@@ -23,3 +23,27 @@ struct dir_entry_info {
 
 u64 namehash(const strbuf<DIRSIZ>&);
 class dirns : public nstbl<strbuf<DIRSIZ>, dir_entry_info, namehash> {};
+
+class dir_entries {
+public:
+  dir_entries(u64 size) : map_(size) {}
+  NEW_DELETE_OPS(dir_entries);
+
+  bool lookup(const strbuf<DIRSIZ>& name, dir_entry_info *de_info_ptr)
+  {
+    return map_.lookup(name, de_info_ptr);
+  }
+
+  bool insert(const strbuf<DIRSIZ>& name, const dir_entry_info& de_info)
+  {
+    return map_.insert(name, de_info);
+  }
+
+  bool remove(const strbuf<DIRSIZ>& name)
+  {
+    return map_.remove(name);
+  }
+
+private:
+  chainhash<strbuf<DIRSIZ>, dir_entry_info> map_;
+};
