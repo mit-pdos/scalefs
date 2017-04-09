mit-pdos/scalefs

commit f7fd9476a1536f3abdebbdacd3769cf6f854bf62

mnode, onzero: Don't flush the transaction queue when deleting the inode
When an mnode's onzero() function is reached and it is marked for deletion, we
construct a transaction that deletes its on-disk inode. However, we don't need
to actually flush the transaction to the on-disk journal and commit it rightaway;
it can be done lazily by a subsequent sync/fsync, possibly reaping the benefits
of group-commit as well. This avoids potentially performing a separate commit
for every inode deletion!

kernel/mnode.cc
@@ -121,15 +121,14 @@ mnode::onzero()
   int cpu = myid();
 
   if (delete_inode_) {
-    // Delete the on-disk inode corresponding to this mnode.
-    auto guard = rootfs_interface->fs_journal[cpu]->commitq_insert_lock.guard();
-    transaction *tr = new transaction();
-    rootfs_interface->delete_mnum_inode_safe(mnum_, tr, true, true);
     rootfs_interface->free_metadata_log(mnum_);
     rootfs_interface->free_mnode_lock(mnum_);
+
+    // Delete the on-disk inode corresponding to this mnode.
+    transaction *tr = new transaction();
+    auto guard = rootfs_interface->fs_journal[cpu]->commitq_insert_lock.guard();
+    rootfs_interface->delete_mnum_inode_safe(mnum_, tr, true, true);
     rootfs_interface->add_transaction_to_queue(tr, cpu);
-    guard.release();
-    rootfs_interface->flush_transaction_queue(cpu);
   }
 
   if (type() == types::file)

commit 655ef7abbd4bf947f2e1571d51f0c82ad74acd92

scalefs, mnode: Fix deadlock caused by skipping transaction flush from mnode::onzero()
As an optimization, commit f7fd9476a decided not to flush the transaction queue
after enqueuing a transaction to delete inodes from mnode::onzero(). While this
is correct in principle, the journaling code has slightly different
expectations: it expects that any new transaction that is enqueued on a per-core
transaction queue is driven all the way up to the point of being committed to
that core's on-disk journal. That way, when there are cross-queue dependencies
between transactions, one simply has to wait for the appropriate transaction to
get committed, before proceeding to commit the dependent transaction; in
particular, we don't have to poke the other transaction queue or force-flush its
transactions to disk.

Commit f7fd9476a broke this assumption by enqueuing a transaction and just
leaving it in the queue, instead of seeing it through all the way upto commit.
As a result, it causes the code to hang whenever a transaction from a different
per-core queue ends up waiting for this (abandoned) transaction to commit.

Fix it by having a different scheme to implement the optimization: let
mnode::onzero() note down the inode number to be deleted in a per-core
data-structure, so that a subsequent sync or fsync on that core can delete those
inodes and also drive them up to the commit point.

include/scalefs.hh
@@ -868,6 +868,15 @@ class mfs_interface
     // transaction-queue that transaction went into and at what timestamp).
     chainhash<u32, tx_queue_info> *blocknum_to_queue;
 
+    // List of mnums whose mnodes have hit mnode::onzero() and hence their
+    // corresponding on-disk inodes can be deleted.
+    struct delete_inums {
+      std::vector<u64> mnum_list;
+      spinlock lock;
+    };
+    percpu<delete_inums> delete_inums;
+
+
   private:
     chainhash<u64, mfs_logical_log*> *metadata_log_htab; // The logical log
 

kernel/mnode.cc
@@ -124,11 +124,9 @@ mnode::onzero()
     rootfs_interface->free_metadata_log(mnum_);
     rootfs_interface->free_mnode_lock(mnum_);
 
-    // Delete the on-disk inode corresponding to this mnode.
-    transaction *tr = new transaction();
-    auto guard = rootfs_interface->fs_journal[cpu]->commitq_insert_lock.guard();
-    rootfs_interface->delete_mnum_inode_safe(mnum_, tr, true, true);
-    rootfs_interface->add_transaction_to_queue(tr, cpu);
+    // Mark this inode for lazy deletion.
+    auto l = rootfs_interface->delete_inums[cpu].lock.guard();
+    rootfs_interface->delete_inums[cpu].mnum_list.push_back(mnum_);
   }
 
   if (type() == types::file)

kernel/scalefs.cc
@@ -588,6 +588,22 @@ mfs_interface::process_metadata_log_and_flush(int cpu)
 
   sync_dirty_files_and_dirs(cpu, mnum_list);
 
+  for (int i = 0; i < NCPU; i++)  {
+    auto commit_insert_guard = fs_journal[i]->commitq_insert_lock.guard();
+
+    // Delete all the inodes marked for lazy deletion by mnode::onzero()
+    std::vector<u64> del_mnum_list;
+    {
+      auto l = delete_inums[i].lock.guard();
+      del_mnum_list = std::move(delete_inums[i].mnum_list);
+    }
+    for (auto &del_mnum : del_mnum_list) {
+      transaction *tr = new transaction();
+      delete_mnum_inode_safe(del_mnum, tr, true, true);
+      add_transaction_to_queue(tr, i);
+    }
+  }
+
   // Commit and apply pending transactions from ALL the per-core queues, not
   // just the queue we added transactions to above.
   for (int i = 0; i < NCPU; i++)
@@ -1287,6 +1303,20 @@ mfs_interface::process_metadata_log(u64 max_tsc, u64 mnode_mnum, int cpu)
   int ret;
 
   auto commit_insert_guard = fs_journal[cpu]->commitq_insert_lock.guard();
+
+  // Delete all the inodes marked for lazy deletion by mnode::onzero()
+  {
+    std::vector<u64> del_mnum_list;
+    {
+      auto l = delete_inums[cpu].lock.guard();
+      del_mnum_list = std::move(delete_inums[cpu].mnum_list);
+    }
+    for (auto &del_mnum : del_mnum_list) {
+      transaction *tr = new transaction();
+      delete_mnum_inode_safe(del_mnum, tr, true, true);
+      add_transaction_to_queue(tr, cpu);
+    }
+  }
 
   pending_stack.push_back({mnode_mnum, max_tsc, -1});
 