9d9c630b66c1bd7564b8a9841e67bffd1bee6cff
Srivatsa S. Bhat <srivatsa@csail.mit.edu>
2017-04-21 09:48:23 -0400

    dbench: Build fixes for dbench/util.c

--- dbench/util.c
+++ dbench/util.c
@@ -18,54 +18,6 @@
 
 #include "dbench.h"
 
-#ifndef SHM_W
-#define SHM_W 0000200
-#endif
-
-#ifndef SHM_R
-#define SHM_R 0000400
-#endif
-
-/* return a pointer to a anonymous shared memory segment of size "size"
-   which will persist across fork() but will disappear when all processes
-   exit 
-
-   The memory is zeroed 
-
-   This function uses system5 shared memory. It takes advantage of a property
-   that the memory is not destroyed if it is attached when the id is removed
-   */
-void *shm_setup(int size)
-{
-	int shmid;
-	void *ret;
-
-	shmid = shmget(IPC_PRIVATE, size, SHM_R | SHM_W);
-	if (shmid == -1) {
-		printf("can't get private shared memory of %d bytes: %s\n",
-		       size, 
-		       strerror(errno));
-		exit(1);
-	}
-	ret = (void *)shmat(shmid, 0, 0);
-	if (!ret || ret == (void *)-1) {
-		printf("can't attach to shared memory\n");
-		return NULL;
-	}
-	/* the following releases the ipc, but note that this process
-	   and all its children will still have access to the memory, its
-	   just that the shmid is no longer valid for other shm calls. This
-	   means we don't leave behind lots of shm segments after we exit 
-
-	   See Stevens "advanced programming in unix env" for details
-	   */
-	shmctl(shmid, IPC_RMID, 0);
-
-	memset(ret, 0, size);
-	
-	return ret;
-}
-
 /****************************************************************************
 similar to string_sub() but allows for any character to be substituted. 
 Use with caution!
@@ -188,11 +140,5 @@ unsigned long timeval_elapsed2(struct timeval *tv1, struct timeval *tv2)
 **/
 void msleep(unsigned int t)
 {
-	struct timeval tval;  
-
-	tval.tv_sec = t/1000;
-	tval.tv_usec = 1000*(t%1000);
-	/* this should be the real select - do NOT replace
-	   with sys_select() */
-	select(0,NULL,NULL,NULL,&tval);
+	usleep(t * 1000);
 }
